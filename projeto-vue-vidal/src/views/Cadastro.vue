ce7c10cc4e34e53159deeb5c5e24707f1064c295
vidalreis <99936406+vidalreis@users.noreply.github.com>
2024-11-15 00:56:43 +0000

    Novoss arquivos

---
+++ projeto-vue-vidal/src/views/Cadastro.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cadastro">
+    <h1>Cadastro de Itens</h1>
+    <form @submit.prevent="cadastrarItem">
+      <div>
+        <label for="nome">Nome do Item:</label>
+        <input type="text" id="nome" v-model="nome" required />
+      </div>
+      <div>
+        <label for="descricao">Descrição:</label>
+        <input type="text" id="descricao" v-model="descricao" required />
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: '',
+      descricao: '',
+    };
+  },
+  methods: {
+    cadastrarItem() {
+      const novoItem = {
+        nome: this.nome,
+        descricao: this.descricao,
+      };
+      // Salvar o item na listagem global (pode ser no Vuex ou em um localStorage)
+      this.$router.push({ name: 'listagem' });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos para o formulário */
+</style>
---
+++ projeto-vue-vidal/src/views/Listagem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="listagem">
+    <h1>Listagem de Itens</h1>
+    <ul>
+      <li v-for="(item, index) in itens" :key="index">
+        {{ item.nome }} - {{ item.descricao }}
+        <button @click="deletarItem(index)">Deletar</button>
+      </li>
+    </ul>
+    <router-link to="/cadastro">Cadastrar Novo Item</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      itens: JSON.parse(localStorage.getItem('itens') || '[]'),
+    };
+  },
+  methods: {
+    deletarItem(index) {
+      this.itens.splice(index, 1);
+      localStorage.setItem('itens', JSON.stringify(this.itens));
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos para a listagem */
+</style>
